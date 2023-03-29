f5b4020baf154c5dfc7f8955017228b5f05d11ca
François <fdiouf696@gmail.com>
2023-03-29 12:02:43 +0200

    version avec horloge

--- transportsNantes.js
+++ transportsNantes.js
@@ -160,3 +160,19 @@ async function fetchRoads() {
 }
 
 fetchRoads()
+
+function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    m = checkTime(m);
+    document.getElementById('time').innerHTML =
+    "Heure : "+h + ":" + m;
+    var t = setTimeout(startTime, 500);
+}
+function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+}
+
+startTime();
